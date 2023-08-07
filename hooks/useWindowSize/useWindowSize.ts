2f1478111dfc4a14bbbeb50791264c729b759fb5
Cliff Likovo <clifflikovodeveloper@gmail.com>
2023-08-07 23:53:58 +0200

    update useWindowSize

--- hooks/useWindowSize/useWindowSize.ts
+++ hooks/useWindowSize/useWindowSize.ts
@@ -4,15 +4,19 @@ import React from 'react'
 import { getScreenWidth } from 'utils'
 
 function useWindowSize(): ['lg' | 'md' | 'sm' | 'xs', number] {
+  const initialWidth = typeof window !== 'undefined' ? window.innerWidth : 0
   const [windowSize, setWindowSize] = React.useState<
     ['lg' | 'md' | 'sm' | 'xs', number]
-  >(getScreenWidth(window.innerWidth))
+  >(getScreenWidth(initialWidth))
 
   React.useEffect(() => {
-    window.addEventListener('resize', () => {
-      const width = window.innerWidth
-      setWindowSize(getScreenWidth(width))
-    })
+    if (typeof window !== 'undefined') {
+      // Only access window object in the browser environment
+      window.addEventListener('resize', () => {
+        const width = window.innerWidth
+        setWindowSize(getScreenWidth(width))
+      })
+    }
   }, [])
 
   return windowSize
--- skeletons/DetailSkeleton/DetailSkeleton.tsx
+++ skeletons/DetailSkeleton/DetailSkeleton.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { SquareSkeleton, CardCarouselSkeleton } from 'skeletons'
 
