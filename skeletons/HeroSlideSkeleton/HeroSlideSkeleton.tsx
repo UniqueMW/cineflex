a274b4b26e71542a592c392c074c3fec0c389437
Cliff Likovo <clifflikovodeveloper@gmail.com>
2023-08-17 01:32:08 +0200

    fix heroslide skeleton

--- skeletons/HeroSlideSkeleton/HeroSlideSkeleton.tsx
+++ skeletons/HeroSlideSkeleton/HeroSlideSkeleton.tsx
@@ -5,7 +5,7 @@ function HeroSlideSkeleton(): JSX.Element {
   return (
     <div className="absolute lg:top-32 lg:left-10 lg:bottom-0 left-4 bottom-16 z-10 space-y-4 w-fit">
       <SquareSkeleton width="128px" height="40px" className="z-10" />
-      <SquareSkeleton width="40%" height="60px" className="z-10" />
+      <SquareSkeleton width="100%" height="60px" className="z-10" />
       <SquareSkeleton width="128px" height="40px" className="z-10" />
       <SquareSkeleton width="160px" height="40px" className="z-10" />
       <SquareSkeleton
